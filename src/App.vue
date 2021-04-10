a7d7365d54bdc2137fff443d5f1e98ed8490d3d4
NaldoColella <naldo.colella@gmail.com>
2021-04-10 19:19:55 -0300

    Creating App.Vue main screen

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+	</div>
+</template>
+
+<script>
+
+
+export default {
+	data() {
+		return {
+			message: 'Setting up project structure.',
+			users: []
+		};
+	},
+};
+</script>
+
+<style>
+#app {
+	
+}
+</style>
