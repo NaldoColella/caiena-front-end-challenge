35d07c99cc71ea770dfc16f213549b3f9a8357ba
NaldoColella <naldo.colella@gmail.com>
2021-04-10 20:14:51 -0300

    minor adjust in Api to receive name and page

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,11 +1,11 @@
 export default {
-    getUsers(){
+    getUsers(name, page = 1){
         //this.responseAvailable = false;
         let url = new URL("https://api.github.com/search/users"),
         params = {
-            q:'test',
+            q: name,
             per_page: '20',
-            page: '1',
+            page: page,
             order: 'asc'
         };
         Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
