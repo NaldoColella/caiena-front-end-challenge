6f824dcad033621cf76398f56403dc87c27faee0
NaldoColella <naldo.colella@gmail.com>
2021-04-10 21:46:27 -0300

    Minor adjustment in UserCard

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,11 +1,31 @@
 <template>
-    <p>{{user}}</p>
+    <div class='UserCard'>
+        <img v-bind:src="user.avatar_url"/>
+        <p>{{user.login}}</p>
+        <a v-bind:href="user.html_url">Profile</a>
+    </div>
 </template>
 
 <script>
 export default {
     props:{
-        user: { required: true }
+        user: { 
+            type: Object,
+            required: true
+        }
     }
 }  
 </script>
+<style>
+    .UserCard {
+        width: 20%;        
+        padding: 0px 2% 0px 2%;
+        box-sizing: border-box;
+    }
+    .UserCard img {
+        border-radius: 50%;
+    }
+    .UserCard img, .UserCard p, .UserCard a {
+        width:100%;
+    }
+</style>
